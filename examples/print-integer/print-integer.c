5db10c6a72c2f898c7b9d8624d095ea6cb5be460
PHPRHiNOS <vanbeek@drupalsailor.com>
2020-12-23 13:10:08 -0500

    Create print-integer.c

---
+++ examples/print-integer/print-integer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %di\n", number);
+    
+    return 0;
+}
+
